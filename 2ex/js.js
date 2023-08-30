463d666544c88c8203de6c06d527bb7f7a4a7b10
YuHuro <128969811+YuHuro@users.noreply.github.com>
2023-08-30 18:46:58 +0300

    Update js.js

--- 2ex/js.js
+++ 2ex/js.js
@@ -6,7 +6,7 @@ function submitText(){
     if(input.value === ''){
         block.innerHTML += '<h1>' + ' Поле обязательно для заполнения!' + '</h1>'
     }
-    if(input.value.length < 3){
+    if(input.value.length < 3 & input.value.length){
         block.innerHTML += '<h1>' + 'Поле должно содержать не менее 3-х символов' + '</h1>'
     }else{
         block.innerHTML += '<h1>' + input.value + '</h1>'
